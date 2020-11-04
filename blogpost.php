5be3992782ef0205094e46c2deaeb75934b4c3a9
Tanushree28 <tanu.nepal1@gmail.com>
2020-11-04 19:03:01 +0545

    blog

---
+++ blogpost.php
@@ -0,0 +1,35 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+  <h2>WELCOME TO YOUR PROFILE</h2>
+   
+ <!-- <form method="post" action="index.php">
+    <?php include('errors.php'); ?>
+    <div class="input-group">
+      <label>Username</label>
+      <input type="text" name="username" >
+    </div>
+    <div class="input-group">
+      <label>Password</label>
+      <input type="password" name="password">
+    </div>
+    <div class="input-group">
+      <button type="submit" class="btn" name="login_user">Login</button>
+    </div>
+    <p>
+      Not yet a member?<div id= "signup container">  
+        <a href="register.php">Sign up</a>
+      </div>
+    </p>
+    <center>
+      <a href="forget-password.php">Forgot Password?</a>
+    </center>
+  </form> -->
+
+</body>
+</html>
